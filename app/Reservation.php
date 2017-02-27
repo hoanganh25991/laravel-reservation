e7ef5c0135da0abb3e1b576bb6cc74f77944a218
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-02-27 09:10:16 +0700

    Reservation State

--- app/Reservation.php
+++ app/Reservation.php
@@ -4,7 +4,14 @@ namespace App;
 
 class Reservation extends HoiModel
 {
+    const RESERVED = 100;
+    const REMINDER_SENT = 200;
+    const CONFIRMED = 300;
+    const ARRIVED = 400;
+    const USER_CANCELLED = -100;
+    const STAFF_CANCELLED = -200;
+    const NO_SHOW = -300;
+
     protected $table = 'reservation';
 
-    
 }
