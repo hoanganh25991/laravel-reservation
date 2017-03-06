033d8e13a32deccc8445f9ea2e4664aa941990b0
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-03-06 13:43:46 +0700

    Reservation > booking form html

--- resources/views/reservations/booking-form.blade.php
+++ resources/views/reservations/booking-form.blade.php
@@ -6,25 +6,17 @@
     <div class="box">
         <div class="hdr-wrapper">
             <div class="header cf">
-
                 <div class="logo">
-                    <a href="https://www.chope.co/singapore-restaurants" target="_blank"><img src="https://d2jzxcrnybzkkt.cloudfront.net/static/widget_v4/img/logo.png?date=201702171400"></a>
+                    <a href="{{ url('') }}" target="_blank"> <img src="{{ url('images/logo.png') }}"> </a>
                 </div>
-
-                <h5 class="title">
-                    Make a Reservation at									 <span class="r-name"> <a href="https://www.chope.co/singapore-restaurants/categories/restaurant/spize-bedok" target="_blank">Spize (Bedok)</a></span>
-
+                <h5 class="title">Make a Reservation at <span class="r-name"> <a href="{{ url('') }}" target="_blank">Spize (Bedok)</a></span>
                 </h5>
-
             </div>
         </div>
         <div id="check-availability" class="content">
             <div class="rid-select">
-                <input name="switch_hn1" id="switch_hn1" value="0" type="hidden">
-                <input name="switch_select_location" id="switch_select_location" value="0" type="hidden">
                 <select name="rid" id="rid" title="spize" class="form-control">
-                    <option value="select_location">Select Location</option>
-                    <option selected="" value="spizebedok1504spb">Spize (Bedok)</option>
+                    <option value="spizebedok1504spb" selected>Spize (Bedok)</option>
                     <option value="spizerivervalley1505srv">Spize (River Valley)</option>
                     <option value="spizeriflerange1510srr">Spize (Rifle Range)</option>
                 </select>
@@ -38,8 +30,8 @@
             <div class="selectors cf">
                 <span id="adults-wrap">
                    <label for="adults">Adults</label>
-                   <select name="adults" id="adults" class="form-control" title="No of Adults">
-                      <option value="5" selected="">5</option>
+                   <select name="adult_pax" class="form-control">
+                      <option value="5" selected>5</option>
                       <option value="6">6</option>
                       <option value="7">7</option>
                       <option value="8">8</option>
@@ -50,17 +42,12 @@
                       <option value="13">13</option>
                       <option value="14">14</option>
                       <option value="15">15</option>
-                      <option value="16">16</option>
-                      <option value="17">17</option>
-                      <option value="18">18</option>
-                      <option value="19">19</option>
-                      <option value="20">20</option>
                    </select>
                 </span>
                 <span id="children-wrap">
                    <label for="children">Children</label>
-                   <select name="children" id="children" class="form-control" title="">
-                      <option value="0" selected="">0</option>
+                   <select name="children_pax" class="form-control">
+                      <option value="0" selected>0</option>
                       <option value="1">1</option>
                       <option value="2">2</option>
                       <option value="3">3</option>
@@ -70,12 +57,6 @@
                       <option value="7">7</option>
                       <option value="8">8</option>
                       <option value="9">9</option>
-                      <option value="10">10</option>
-                      <option value="11">11</option>
-                      <option value="12">12</option>
-                      <option value="13">13</option>
-                      <option value="14">14</option>
-                      <option value="15">15</option>
                    </select>
                 </span>
             </div>
@@ -83,27 +64,25 @@
                 <div class="clear"></div>
                 <div id="calendar-box" align="center"></div>
                 <div id="dt-choice" class="cf">
-                    <input name="date" type="hidden" id="datepicker_value" value="3-3-2017" readonly="">
-                    <input name="datepicker_date_value" type="hidden" id="datepicker_date_value" value="3" readonly="">
-                    <label id="sel-date">  Mar 03 2017</label>
-                    <input id="sel-date_2" type="hidden" value="03 3 2017">
-                    <input id="sel-date_3" type="hidden" value="03 3 2017">
-                    <select id="time" name="time" class="form-control" title="Choose a Time">
+                    <label id="reservation_time">Mar 03 2017</label>
+                    <select name="time" class="form-control">
                         <option>N/A</option>
                     </select>
-                    <input id="wait_list" name="wait_list" value="0" type="hidden">
-                    <input id="reservation_charge" name="reservation_charge" value="0" type="hidden">
                 </div>
-                <div class="agree-box hidden cf">
+                <div class="agree-box cf">
                     <div class="checkbox cf">
-                        <input id="check-required0" type="checkbox" class="form-control agree-check" name="agree_box" value="1">
-                        <label for="check-required0">I acknowledge that this is a waitlisted reservation and is subjected to the restaurant's confirmation.</label>
+                        <label for="agree_box">I acknowledge that this is a waitlisted reservation and is subjected to
+                            the restaurant's confirmation.</label>
+                        <input id="agree_box" type="checkbox" name="agree_box" value="1"
+                               class="form-control agree-check" required>
                     </div>
                 </div>
             </div>
             <div class="form-actions cf" id="bottom_room">
                 <button type="button" id="back_button" class="btn" style="float:left">Previous</button>
-                <button type="submit" id="index_submit_button_id" class="btn btn-primary" style="float:right" disabled="disabled">Next</button>
+                <button type="submit" id="index_submit_button_id" class="btn btn-primary" style="float:right"
+                        disabled="disabled">Next
+                </button>
             </div>
         </div>
     </div>
