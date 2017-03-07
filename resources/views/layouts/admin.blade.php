9713deec28fd580fb2a9a7440172ad1367848170
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-03-07 16:20:36 +0700

    Admin page html

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,74 +13,17 @@
         <div class="col-md-10 col-md-offset-2">
             <div id="main-navigator">
                 <ul class="nav nav-tabs">
-                    <li><a href="#reservations" data-toggle="tab">Home</a></li>
-                    <li><a href="#customers" data-toggle="tab">Profile</a></li>
-                    <li><a href="#settings" data-toggle="tab">Messages</a></li>
+                    <li><a href="#reservations" data-toggle='tab'>Reservations</a></li>
+                    <li><a href="#customers"    data-toggle='tab'>Customers</a></li>
+                    <li><a href="#settings"     data-toggle='tab'>Settings</a></li>
                 </ul>
             </div>
         </div>
     </div>
-    <div class="row bhoechie-tab-container">
-        <div class="col-md-2 bhoechie-tab-menu">
-            <div class="list-group">
-                <a href="#" class="list-group-item active text-center">
-                    <h4 class="glyphicon glyphicon-plane"></h4><br/>Flight
-                </a>
-                <a href="#" class="list-group-item text-center">
-                    <h4 class="glyphicon glyphicon-road"></h4><br/>Train
-                </a>
-                <a href="#" class="list-group-item text-center">
-                    <h4 class="glyphicon glyphicon-home"></h4><br/>Hotel
-                </a>
-                <a href="#" class="list-group-item text-center">
-                    <h4 class="glyphicon glyphicon-cutlery"></h4><br/>Restaurant
-                </a>
-                <a href="#" class="list-group-item text-center">
-                    <h4 class="glyphicon glyphicon-credit-card"></h4><br/>Credit Card
-                </a>
-            </div>
-        </div>
-        <div class="col-md-10 bhoechie-tab">
-            <!-- flight section -->
-            <div class="bhoechie-tab-content active">
-                <center>
-                    <h1 class="glyphicon glyphicon-plane" style="font-size:14em;color:#55518a"></h1>
-                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-                    <h3 style="margin-top: 0;color:#55518a">Flight Reservation</h3>
-                </center>
-            </div>
-            <!-- train section -->
-            <div class="bhoechie-tab-content">
-                <center>
-                    <h1 class="glyphicon glyphicon-road" style="font-size:12em;color:#55518a"></h1>
-                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-                    <h3 style="margin-top: 0;color:#55518a">Train Reservation</h3>
-                </center>
-            </div>
-
-            <!-- hotel search -->
-            <div class="bhoechie-tab-content">
-                <center>
-                    <h1 class="glyphicon glyphicon-home" style="font-size:12em;color:#55518a"></h1>
-                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-                    <h3 style="margin-top: 0;color:#55518a">Hotel Directory</h3>
-                </center>
-            </div>
-            <div class="bhoechie-tab-content">
-                <center>
-                    <h1 class="glyphicon glyphicon-cutlery" style="font-size:12em;color:#55518a"></h1>
-                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-                    <h3 style="margin-top: 0;color:#55518a">Restaurant Diirectory</h3>
-                </center>
-            </div>
-            <div class="bhoechie-tab-content">
-                <center>
-                    <h1 class="glyphicon glyphicon-credit-card" style="font-size:12em;color:#55518a"></h1>
-                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-                    <h3 style="margin-top: 0;color:#55518a">Credit Card</h3>
-                </center>
-            </div>
-        </div>
+    <div class="tab-content">
+        <div class="row tab-pane" id="reservations">Reservations</div>
+        <div class="row tab-pane" id="customers">Customers</div>
+        <div class="row tab-pane active" id="settings">Settings</div>
     </div>
 </div>
 <script src="{{ url('js/jquery.min.js') }}"></script>
@@ -88,16 +31,6 @@
 <script src="{{ url('js/moment.min.js') }}"></script>
 @stack('script')
 <script type="text/javascript">
-    $(document).ready(function(){
-        $("div.bhoechie-tab-menu>div.list-group>a").click(function(e){
-            e.preventDefault();
-            $(this).siblings('a.active').removeClass("active");
-            $(this).addClass("active");
-            var index = $(this).index();
-            $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
-            $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
-        });
-    });
 </script>
 </body>
 </html>
