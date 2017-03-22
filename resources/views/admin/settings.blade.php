ec15a3409590ff6b03ee5833940b25af9bda002c
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-03-22 16:34:55 +0700

    Admin page, Weekly session edit mode, input

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -257,7 +257,8 @@
                                                         <label class="switch">
                                                             <input type="checkbox"
                                                                    :checked="(timing.disabled == 0) ? 'checked' : false"
-                                                                   :id="'timing_' + timing.id + 'disabled'">
+                                                                   :id="'timing_' + timing.id + 'disabled'"
+                                                                   v-model="timing['disabled']">
                                                             <div class="slider round"></div>
                                                         </label>
                                                     </td>
@@ -265,65 +266,69 @@
                                                         <input type="text"
                                                                style="width: 60px"
                                                                :id="'timing_' + timing.id + 'timing_name'"
-                                                               :value="timing.timing_name">
+                                                               :value="timing.timing_name"
+                                                               v-model="timing['timing_name']">
                                                     </td>
                                                     <td>
                                                         <input type="time"
                                                                style="width: 65px; height: 20px"
                                                                :id="'timing_' + timing.id + 'first_arrival_time'"
-                                                               :value="timing.first_arrival_time">
+                                                               :value="timing.first_arrival_time"
+                                                               v-model="timing['first_arrival_time']">
                                                     </td>
                                                     <td>
                                                         <input type="time"
                                                                style="width: 65px; height: 20px"
                                                                :id="'timing_' + timing.id + 'last_arrival_time'"
-                                                               :value="timing.last_arrival_time">
+                                                               :value="timing.last_arrival_time"
+                                                               v-model="timing['last_arrival_time']">
                                                     </td>
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'interval_minutes'"
-                                                               :value="timing.interval_minutes">
+                                                               v-model="timing['interval_minutes']">
                                                     </td>
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'capacity_1'"
-                                                               :value="timing.capacity_1">
+                                                               v-model="timing['capacity_1']">
                                                     </td>
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'capacity_2'"
-                                                               :value="timing.capacity_2">
+                                                               v-model="timing['capacity_2']">
                                                     </td>
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'capacity_3_4'"
-                                                               :value="timing.capacity_3_4">
+                                                               v-model="timing['capacity_3_4']">
                                                     </td>
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'capacity_5_6'"
-                                                               :value="timing.capacity_5_6"></td>
+                                                               v-model="timing['capacity_5_6']">
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'capacity_7_x'"
-                                                               :value="timing.capacity_7_x">
+                                                               v-model="timing['capacity_7_x']">
                                                     </td>
                                                     <td>
-                                                        <input type="text"
-                                                               style="width: 20px"
+                                                        <input type="number"
+                                                               style="width: 40px"
                                                                :id="'timing_' + timing.id + 'max_pax'"
-                                                               :value="timing.max_pax">
+                                                               v-model="timing['max_pax']">
                                                     </td>
                                                     <td>
                                                         <input type="checkbox"
                                                                :checked="(timing.children_allowed == 1) ? 'checked' : false"
-                                                               :id="'timing_' + timing.id + 'children_allowed'">
+                                                               :id="'timing_' + timing.id + 'children_allowed'"
+                                                               v-model="timing['children_allowed']">
                                                         <label style="width: 12px"
                                                                :for="'timing_' + timing.id + 'children_allowed'">
                                                         </label>
