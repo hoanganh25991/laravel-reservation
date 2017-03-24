5a76e5d6e3df984dc3954c63af1148f4978c54ae
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-03-24 12:05:34 +0700

    Admin page, Setting manage on user, html

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -208,6 +208,37 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h4 class="bg-info">User list</h4>
+                                    <table class="table table-striped">
+                                        <thead>
+                                        <tr>
+                                            <th>Firstname</th>
+                                            <th>Lastname</th>
+                                            <th>Email</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>John</td>
+                                            <td>Doe</td>
+                                            <td>john@example.com</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Mary</td>
+                                            <td>Moe</td>
+                                            <td>mary@example.com</td>
+                                        </tr>
+                                        <tr>
+                                            <td>July</td>
+                                            <td>Dooley</td>
+                                            <td>july@example.com</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                         <div class="modal-footer" style="border-top: 1px solid #e5e5e5;">
                             <button v-on:click="_updateSettings"
