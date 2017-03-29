aac0a938ad36ef2bdae68723815aed23257abe57
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-03-29 16:39:28 +0700

    Booking form, change dialog title

--- resources/views/reservations/booking-form.blade.php
+++ resources/views/reservations/booking-form.blade.php
@@ -144,7 +144,7 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                                 aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Searching for available time</h4>
+                    <h4 class="modal-title">Sending reservation...</h4>
                 </div>
                 <div class="modal-body center">
                     {{--<div style="width: 100px; display: inline-block">--}}
