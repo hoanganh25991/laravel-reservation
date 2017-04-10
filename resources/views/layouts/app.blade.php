d6f818148d04e029bcdc4b48add14038aa1edc40
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-04-10 12:06:01 +0700

    Fit page into iframe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html >
 <head>
     <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
     <link href="{{ url('css/font-awesome.min.css') }}" rel="stylesheet" media="screen">
     <link href="{{ url(substr(mix('css/reservation.css'), 1)) }}" rel="stylesheet">
     @stack('css')
 </head>
-<body>
+<body style="width: 468px; height: 670px; overflow: hidden;">
 
 <div class="container">
     @yield('content')
