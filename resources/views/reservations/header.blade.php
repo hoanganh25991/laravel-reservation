5271e9118e34b37a316adade00726afec63734b4
Torin Nguyen <torinnguyen@gmail.com>
2017-04-10 15:04:05 +0700

    Remove header

--- resources/views/reservations/header.blade.php
+++ resources/views/reservations/header.blade.php
@@ -1,10 +1,5 @@
 <div class="hdr-wrapper">
     <div class="header cf">
-        <div class="logo"  style="padding: 10px">
-            <a href="{{ url('') }}" target="_blank"><img src="{{ url('images/logo.png') }}"></a>
-        </div>
-        <h5 class="title">
-            {{ $title }}
-        </h5>
+        
     </div>
 </div>
