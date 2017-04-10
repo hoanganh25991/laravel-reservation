704a55946b4347e113d15efaa56045f24e163642
Torin Nguyen <torinnguyen@gmail.com>
2017-04-10 16:46:13 +0700

    Forget to add flat-ui js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,6 +20,7 @@
 </div>
 <script src="{{ url('js/jquery.min.js') }}"></script>
 <script src="{{ url('js/bootstrap.min.js') }}"></script>
+<script src="{{ url('js/flat-ui.min.js') }}"></script>
 <script src="{{ url('js/moment.min.js') }}"></script>
 <script src="{{ url('js/redux.min.js') }}"></script>
 @stack('script')
