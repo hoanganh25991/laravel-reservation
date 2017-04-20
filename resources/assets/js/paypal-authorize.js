3fe062a65dbf90630a987318a338638e9acf9c18
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-04-20 10:41:27 +0700

    Try rewrite booking form, paypal payment in deposit case

--- resources/assets/js/paypal-authorize.js
+++ resources/assets/js/paypal-authorize.js
@@ -5,8 +5,8 @@ const AJAX_PAYMENT_REQUEST_FIND_RESERVATION_FAIL = 'AJAX_PAYMENT_REQUEST_FIND_RE
 const AJAX_PAYMENT_REQUEST_TRANSACTION_FAIL = 'AJAX_PAYMENT_REQUEST_TRANSACTION_FAIL';
 class PayPalAuthorize {
 
-	constructor(token, paypal_instance_options, base_url){
-		this.paypal_instance_options =
+	constructor(paypal_token, paypal_options, base_url){
+		this.paypal_options =
 			Object.assign({
 				flow: 'checkout', // Required
 				amount: 10.00, // Required
@@ -24,10 +24,9 @@ class PayPalAuthorize {
 					state: 'IL',
 					phone: '123.456.7890'
 				}
-			}, paypal_instance_options);
+			}, paypal_options);
 
-		this.token = token;
-
+		this.token    = paypal_token;
 		this.base_url = base_url;
 		
 		this.initPaypal();
@@ -37,11 +36,9 @@ class PayPalAuthorize {
 		let self = this;
 		// Fetch the button you are using to initiate the PayPal flow
 		var paypalButton = document.getElementById('paypal-button');
-
 		// Create a Client component
 		//noinspection JSUnresolvedVariable
 		braintree.client.create({
-			//		authorization: 'TOKEN'
 			authorization: self.token
 		}, function(clientErr, clientInstance) {
 			// Create PayPal component
@@ -49,55 +46,68 @@ class PayPalAuthorize {
 			braintree.paypal.create({
 				client: clientInstance
 			}, function(err, paypalInstance) {
-				if(err){
-					console.log(err);
-					throw err;
-				}
+				if(err){throw err;}
 				console.log('paypalInstance', paypalInstance);
 				paypalButton.addEventListener('click', function() {
 					// Tokenize here!
-					paypalInstance.tokenize(self.paypal_instance_options, function(err, tokenizationPayload) {
+					paypalInstance.tokenize(self.paypal_options, function(err, tokenizationPayload) {
 						// Tokenization complete
 						// Send tokenizationPayload.nonce to server
-						if(err){
-							console.log(err);
-							throw err;
-						}
+						if(err){throw err;}
 						console.log('tokenizationPayload', tokenizationPayload);
 						
 						let data = 
 							Object.assign({
 								tokenizationPayload: JSON.stringify(tokenizationPayload),
 								type: AJAX_PAYMENT_REQUEST
-							}, self.paypal_instance_options);
+							}, self.paypal_options);
 						
 						$.ajax({
 							url: self.base_url,
 							method: 'POST',
-							data: data,
+							data,
 							success(res){
 								console.log(res);
-								if(res.statusMsg == AJAX_PAYMENT_REQUEST_SUCCESS){
-									//$('#paypal-dialog').modal('hide');
-									console.log(res);
-									console.log('success payment');
-									document.dispatchEvent(new CustomEvent('PAYPAL_PAYMENT_SUCCESS', {detail: res}));
-									return;
+								switch(res.statusMsg){
+									case AJAX_PAYMENT_REQUEST_SUCCESS:{
+										console.log('%c Success paypal payment', 'background:#FDD835');
+										// Dispatch at document
+										document.dispatchEvent(new CustomEvent('PAYPAL_PAYMENT_SUCCESS', {detail: res}));
+										break;
+									}
+									default:{
+										console.warn('Unknown case of res.statusMsg');
+										break;
+									}
 								}
 							},
-							error(res){
-								if(res.statusMsg == AJAX_PAYMENT_REQUEST_VALIDATE_FAIL
-									|| res.statusMsg == AJAX_PAYMENT_REQUEST_FIND_RESERVATION_FAIL
-									|| res.statusMsg == AJAX_PAYMENT_REQUEST_TRANSACTION_FAIL){
-									let msg = 'PAYPAL FAIL: see log';
-
-									console.log(msg, res.data);
-									window.alert(msg);
-									return;
+							error(res_literal){
+								//noinspection JSUnresolvedVariable
+								console.log(res_literal.responseJSON);
+								// It quite weird that in browser window
+								// Response as status code != 200
+								// res obj now wrap by MANY MANY INFO
+								// Please dont change this
+								let res = res_literal.responseJSON;
+								// Do normal things with res as in success case
+								switch(res.statusMsg){
+									case AJAX_PAYMENT_REQUEST_VALIDATE_FAIL:
+									case AJAX_PAYMENT_REQUEST_FIND_RESERVATION_FAIL:
+									case AJAX_PAYMENT_REQUEST_TRANSACTION_FAIL:{
+										let info = JSON.stringify(res);
+										let msg  = `Paypal payment fail: ${info}`;
+										window.alert(msg);
+										break;
+									}
+									default:{
+										console.warn('Unknown case of res.statusMsg');
+										break;
+									}
 								}
+
 							},
 							complete(res){
-								console.log('response from tokenizationPayload.php', res);
+								//console.log('response from tokenizationPayload.php', res);
 							}
 						});
 					});
