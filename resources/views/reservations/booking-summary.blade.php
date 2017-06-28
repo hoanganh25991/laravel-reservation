717ba28793fe3bd03c13d7868bcca52cac0b3462
Torin Nguyen <torinnguyen@gmail.com>
2017-06-28 21:36:01 +0700

    trying to fix reservations layout on iPad

--- resources/views/reservations/booking-summary.blade.php
+++ resources/views/reservations/booking-summary.blade.php
@@ -15,66 +15,68 @@
                 <label style="float: none;" v-show="selected_outlet.send_sms_on_booking && reservation.status >= 100">An SMS has been sent to your mobile phone</label>
             </p>
         @endif
-        <table id="r-rsrve-info">
-            <tbody>
-            <tr>
-                <td><label>Outlet</label></td>
-                <td>@{{ selected_outlet.outlet_name }}<br/>@{{ selected_outlet.outlet_address }}</td>
-            </tr>
-            <tr>
-                <td style="width: 40%;"><label>Date &amp; Time</label></td>
-                <td v-if="reservation.date">@{{ reservation.date.format('D MMM Y') }} at @{{ reservation.time }}</td>
-                <td v-if="!reservation.date"></td>
-            </tr>
-            <tr>
-                <td><label>Pax</label></td>
-                <td>@{{ reservation.adult_pax }} Adults, @{{ reservation.children_pax }} Children</td>
-            </tr>
-            <tr>
-                <td><label>Name</label></td>
-                <td>@{{ reservation.first_name }} @{{ reservation.last_name }}</td>
-            </tr>
-            <tr>
-                <td><label>Contact Number</label></td>
-                <td>@{{ reservation.phone_country_code }} @{{ reservation.phone }}</td>
-            </tr>
-            <tr>
-                <td><label>Email</label></td>
-                <td>@{{ reservation.email }}</td>
-            </tr>
-            <tr v-show="reservation.customer_remarks != ''">
-                <td><label>Special Request</label></td>
-                <td>@{{ reservation.customer_remarks }}</td>
-            </tr>
-            <!-- When customer success pay money for authorization -->
-            <tr v-show="reservation.payment_status == 100">
-                <td><label>Payment authorization Paid</label></td>
-                <td>
-                    <label class="h5" style="display: block; width: 100%;">@{{ reservation.payment_amount }} (@{{ reservation.payment_currency }})</label>
-                    <div>Your deposit will be returned when you arrive for your reservation</div>
-                </td>
-            </tr>
-            <!-- In case ask customer for authorization payment -->
-            @if($is_summary_page)
-                <tr v-show="reservation.payment_status == 25">
-                    <td><label>Payment authorization Required</label></td>
+        <div style="overflow-x: auto;">
+            <table id="r-rsrve-info">
+                <tbody>
+                <tr>
+                    <td><label>Outlet</label></td>
+                    <td>@{{ selected_outlet.outlet_name }}<br/>@{{ selected_outlet.outlet_address }}</td>
+                </tr>
+                <tr>
+                    <td style="width: 40%;"><label>Date &amp; Time</label></td>
+                    <td v-if="reservation.date">@{{ reservation.date.format('D MMM Y') }} at @{{ reservation.time }}</td>
+                    <td v-if="!reservation.date"></td>
+                </tr>
+                <tr>
+                    <td><label>Pax</label></td>
+                    <td>@{{ reservation.adult_pax }} Adults, @{{ reservation.children_pax }} Children</td>
+                </tr>
+                <tr>
+                    <td><label>Name</label></td>
+                    <td>@{{ reservation.first_name }} @{{ reservation.last_name }}</td>
+                </tr>
+                <tr>
+                    <td><label>Contact Number</label></td>
+                    <td>@{{ reservation.phone_country_code }} @{{ reservation.phone }}</td>
+                </tr>
+                <tr>
+                    <td><label>Email</label></td>
+                    <td>@{{ reservation.email }}</td>
+                </tr>
+                <tr v-show="reservation.customer_remarks != ''">
+                    <td><label>Special Request</label></td>
+                    <td>@{{ reservation.customer_remarks }}</td>
+                </tr>
+                <!-- When customer success pay money for authorization -->
+                <tr v-show="reservation.payment_status == 100">
+                    <td><label>Payment authorization Paid</label></td>
                     <td>
-                        <label class="h5 text-danger">@{{ reservation.deposit }} (@{{ reservation.paypal_currency }})</label>
-                        <div class="agree-box cf" style="margin: 40px 0 10px 0;">
-                            <div class="checkbox cf" style="padding-left: 5px;">
-                                <label for="agree_payment_box">I acknowledge that I will be subject to a cancellation fee of $@{{ reservation.deposit }} if I do not give more than 24-hours cancellation notice or if I do not honour this reservation.</label>
-                                <input id="agree_payment_box" type="checkbox" class="form-control agree-check"
-                                   v-model="reservation.agree_payment_term_condition" v-on:click="_togglePaypalButton">
-                            </div>
-                        </div>
-                        <div id="paypal-container" style="transition: 0.5s all ease-in-out; transform: scale(0,1);" class="pull-left">
-                            @include('paypal.authorize')
-                        </div>
+                        <label class="h5" style="display: block; width: 100%;">@{{ reservation.payment_amount }} (@{{ reservation.payment_currency }})</label>
+                        <div>Your deposit will be returned when you arrive for your reservation</div>
                     </td>
                 </tr>
-            @endif
-            </tbody>
-        </table>
+                <!-- In case ask customer for authorization payment -->
+                @if($is_summary_page)
+                    <tr v-show="reservation.payment_status == 25">
+                        <td><label>Payment authorization Required</label></td>
+                        <td>
+                            <label class="h5 text-danger">@{{ reservation.deposit }} (@{{ reservation.paypal_currency }})</label>
+                            <div class="agree-box cf" style="margin: 40px 0 10px 0;">
+                                <div class="checkbox cf" style="padding-left: 5px;">
+                                    <label for="agree_payment_box">I acknowledge that I will be subject to a cancellation fee of $@{{ reservation.deposit }} if I do not give more than 24-hours cancellation notice or if I do not honour this reservation.</label>
+                                    <input id="agree_payment_box" type="checkbox" class="form-control agree-check"
+                                    v-model="reservation.agree_payment_term_condition" v-on:click="_togglePaypalButton">
+                                </div>
+                            </div>
+                            <div id="paypal-container" style="transition: 0.5s all ease-in-out; transform: scale(0,1);" class="pull-left">
+                                @include('paypal.authorize')
+                            </div>
+                        </td>
+                    </tr>
+                @endif
+                </tbody>
+            </table>
+        </div>
         <div class="form-actions cf bottom_room">
             @if($is_summary_page)
                 {{--@if($is_summary_page && env('APP_ENV') != 'production')--}}
