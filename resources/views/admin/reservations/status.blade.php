2172c7f5d722efc6ba59655458dd9db5aa3b2b94
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-29 11:52:07 +0700

    Admin reservations, fix button layout

--- resources/views/admin/reservations/status.blade.php
+++ resources/views/admin/reservations/status.blade.php
@@ -1,13 +1,15 @@
-<select v-model="reservation.status" style="width: 100%; height: 26px"
-        v-on:change="_autoSave(reservation, 'status')"
->
-  <option value="400" class="bg-success">Arrived</option>
-  <option value="300" class="bg-success">Confirmed</option>
-  <option value="200" class="bg-info">Reminder Sent</option>
-  <option value="100" class="bg-info">Reserved</option>
-  <option value="75" disabled>Amendmented</option>
-  <option value="50" disabled>Payment Required</option>
-  <option value="-100" class="bg-info">User cancelled</option>
-  <option value="-200" class="bg-warning">Staff cancelled</option>
-  <option value="-300" class="bg-danger">No show</option>
-</select>
+<div>
+  <select v-model="reservation.status" style="width: 100%; height: 26px"
+          v-on:change="_autoSave(reservation, 'status')"
+  >
+    <option value="400" class="bg-success">Arrived</option>
+    <option value="300" class="bg-success">Confirmed</option>
+    <option value="200" class="bg-info">Reminder Sent</option>
+    <option value="100" class="bg-info">Reserved</option>
+    <option value="75" disabled>Amendmented</option>
+    <option value="50" disabled>Payment Required</option>
+    <option value="-100" class="bg-info">User cancelled</option>
+    <option value="-200" class="bg-warning">Staff cancelled</option>
+    <option value="-300" class="bg-danger">No show</option>
+  </select>
+</div>
