05c37960c1196d00b43945ff0e9b013251fc69bf
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-07-12 09:40:56 +0700

    Re add flat-ui js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,6 +20,7 @@
     @yield('content')
 </div>
 <script src="{{ url('js/jquery.min.js') }}"></script>
+<script src="{{ url('js/flat-ui.min.js') }}"></script>
 <script src="{{ url('js/vue.min.js') }}"></script>
 <script src="{{ url('js/moment.min.js') }}"></script>
 <script src="{{ url('js/redux.min.js') }}"></script>
