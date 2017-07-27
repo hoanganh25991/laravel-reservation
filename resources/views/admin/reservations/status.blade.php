58cbe98dcc0727772c87cf2bef8349057e8fc99b
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-07-27 10:06:58 +0700

    Admin reservation, option status bigger

--- resources/views/admin/reservations/status.blade.php
+++ resources/views/admin/reservations/status.blade.php
@@ -3,14 +3,14 @@
           v-on:change="_autoSave(reservation, 'status')"
           v-on:click="_getLastStatus(reservation.status)"
   >
-    <option value="400" class="bg-success">Arrived</option>
-    <option value="300" class="bg-success">Confirmed</option>
-    <option value="200" class="bg-info">Reminder Sent</option>
-    <option value="100" class="bg-info">Reserved</option>
-    <option value="75" disabled>Amendmented</option>
-    <option value="50" disabled>Payment Required</option>
-    <option value="-100" class="bg-info">User cancelled</option>
-    <option value="-200" class="bg-warning">Staff cancelled</option>
-    <option value="-300" class="bg-danger">No show</option>
+    <option value="400" class="bg-success hoiH4">Arrived</option>
+    <option value="300" class="bg-success hoiH4">Confirmed</option>
+    <option value="200" class="bg-info hoiH4">Reminder Sent</option>
+    <option value="100" class="bg-info hoiH4">Reserved</option>
+    <option value="75" disabled class="hoiH4">Amendmented</option>
+    <option value="50" disabled class="hoiH4">Payment Required</option>
+    <option value="-100" disabled class="bg-info hoiH4">User cancelled</option>
+    <option value="-200" class="bg-warning hoiH4">Staff cancelled</option>
+    <option value="-300" class="bg-danger hoiH4">No show</option>
   </select>
 </div>
